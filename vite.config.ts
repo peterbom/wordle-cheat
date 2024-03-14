f70e4c0601de0a45c2bf909c31d51d58a9bd14c5
Peter Bomber <peter.bomber@microsoft.com>
2024-03-14 21:57:07 +1300

    build

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
-
-export default defineConfig({
-	plugins: [sveltekit(), checker({ typescript: true })]
-});
